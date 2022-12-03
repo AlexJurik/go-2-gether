9d485ada1cdd9fc8d68ef24a49b59a592f5762b2
Matus Stedl <matuss.moje@gmail.com>
2022-12-03 17:05:36 +0100

    auth page

--- src/app/components/onboarding/onboarding-page.component.ts
+++ src/app/components/onboarding/onboarding-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-onboarding',
@@ -6,4 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./onboarding-page.component.scss']
 })
 export class OnboardingPage {
+
+  constructor(private userService: UserService, private router: Router){}
+  email: string="";
+  password: string="";
+
+
+  login(){
+   const logged = this.userService.login(this.email,this.password);
+    if(logged){
+      this.router.navigateByUrl("/app");
+    }
+  }
 }
